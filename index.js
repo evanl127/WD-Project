c32ea650444742d7fc41f6203de0006ff8a4c5d3
evanl127 <144394321+evanl127@users.noreply.github.com>
2025-01-13 20:31:33 +0000

    Day1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAdAp6ZDnY8SbZworUFgdWg6D9xgjmAXuw",
     authDomain: "bths-problem.firebaseapp.com",
@@ -11,16 +11,14 @@ const firebaseConfig = {
     appId: "1:925726886192:web:a9c09e1667a8a459ee60f5",
     measurementId: "G-STRREJTQ5Z"
 };
-
-  // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 
 
-const loginButton = document.getElementsbyId("sign-in=btn")
+const loginButton = document.getElementById("sign-in-btn")
 
-loginButton.addEventLister("click", function(){
-    
+loginButton.addEventListener("click", function(){
+    console.log("baller")
 })
 const emailInputEl = document.getElementById("email-input")
 const passwordInputEl = document.getElementById("password-input")
