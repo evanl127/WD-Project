e3c83d9bdbce5832ac45e4e0f95083ee927909dc
evanl127 <144394321+evanl127@users.noreply.github.com>
2025-01-13 20:11:47 +0000

    sigma

---
+++ index.js
@@ -0,0 +1,26 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
+const firebaseConfig = {
+    apiKey: "AIzaSyAdAp6ZDnY8SbZworUFgdWg6D9xgjmAXuw",
+    authDomain: "bths-problem.firebaseapp.com",
+    databaseURL: "https://bths-problem-default-rtdb.firebaseio.com",
+    projectId: "bths-problem",
+    storageBucket: "bths-problem.firebasestorage.app",
+    messagingSenderId: "925726886192",
+    appId: "1:925726886192:web:a9c09e1667a8a459ee60f5",
+    measurementId: "G-STRREJTQ5Z"
+};
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+
+const loginButton = document.getElementsbyId("sign-in=btn")
+
+loginButton.addEventLister("click", function(){
+    
+})
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
