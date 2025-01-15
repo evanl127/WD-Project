6ffb779643b46cf86772b3fe92aba735aaa943f6
evanl127 <144394321+evanl127@users.noreply.github.com>
2025-01-15 20:32:49 +0000

    day2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-
+import { createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 const firebaseConfig = {
     apiKey: "AIzaSyAdAp6ZDnY8SbZworUFgdWg6D9xgjmAXuw",
     authDomain: "bths-problem.firebaseapp.com",
@@ -14,11 +16,54 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 
-
 const loginButton = document.getElementById("sign-in-btn")
+const signupButton = document.getElementById("sign-up-btn")
 
 loginButton.addEventListener("click", function(){
-    console.log("baller")
+    let user = emailInputEl;
+    let password = passwordInputEl;
+    signInWithEmailAndPassword();
 })
+signupButton.addEventListener("click",function(){
+    let user = emailInputEl;
+    let password = passwordInputEl;
+    createAccount();
+})
+
 const emailInputEl = document.getElementById("email-input")
 const passwordInputEl = document.getElementById("password-input")
+
+function createAccount(){
+    console.log("Sign up with email and password")
+    const email = emailInputEl.value
+    const password= passwordInputEl.value
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      showLoggedInView()
+    })
+    .catch((error) => {
+      console.error(error.message)
+    });
+}
+
+function signinAccount(){
+    console.log("Sign in with email and password")
+    const email = emailInputEl.value
+    const password= passwordInputEl.value
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      showLoggedInView()
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(error.message)
+    });
+}
+
+function showLoggedInView(){
+    window.location.href = "mainpage.html"
+}
+
